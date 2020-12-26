4488700b5d2a3cdd3203da68b2a35f4bcbfe340a
Christopher D Jativa <cjativa@barnesfoundation.org>
2020-12-26 13:58:51 -0500

    initial commit and working draw

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import Draw from './components/draw/draw';
 
-function App() {
+
+const App = () => {
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Switch>
+
+          {/** Landing page */}
+          <Route path="/draw">
+            <Draw />
+          </Route>
+
+          {/** Sign up page */}
+          <Route path="/sign-up">
+            {/* <SignUp /> */}
+          </Route>
+
+          {/** Login page */}
+          <Route path="/login">
+            {/* <Login /> */}
+          </Route>
+        </Switch>
+      </BrowserRouter>
+
     </div>
-  );
-}
+  )
+};
 
 export default App;
