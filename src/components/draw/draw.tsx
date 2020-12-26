0484591f249f15583d8ce71c4b85ea4981d6c3e7
Christopher D Jativa <cjativa@barnesfoundation.org>
2020-12-26 14:49:50 -0500

    working reset of canvas

--- src/components/draw/draw.tsx
+++ src/components/draw/draw.tsx
@@ -1,92 +1,58 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+
+import DrawCanvas from './drawCanvas';
 
 const Draw = () => {
 
-    const canvasElement = useRef<HTMLCanvasElement>(null);
-    const [context, setContext] = useState<CanvasRenderingContext2D>();
-    const [isDrawing, setIsDrawing] = useState(false);
-    const [previousPosition, setPreviousPosition] = useState({
-        offsetX: 0,
-        offsetY: 0
-    });
+    const [saveOccurred, setSaveOccurred] = useState<boolean>(false);
+    const [resetOccurred, setResetOccurred] = useState<boolean>(false);
 
-    const figureLine = [];
-
-    /** Sets up our canvas and context on component mounting */
+    /** Resets the reset state after a reset occurred */
     useEffect(() => {
-        canvasElement.current!.width = 1000;
-        canvasElement.current!.height = 800;
-        setContext(canvasElement.current!.getContext('2d')!);
-    }, []);
+        if (resetOccurred === true) {
+            setResetOccurred(false);
+        }
+    }, [resetOccurred]);
 
-    /** Once context is ready, styke it */
+    /** Resets the save state after a save occurred */
     useEffect(() => {
-        if (context) {
-            context.lineJoin = 'round';
-            context.lineCap = 'round';
-            context.lineWidth = 5;
+        if (saveOccurred === true) {
+            setSaveOccurred(false);
         }
-    }, [context]);
+    }, [saveOccurred]);
 
-    /** Function that handles drawing on mouse down */
-    const onMouseDown = (event: React.MouseEvent<HTMLCanvasElement, MouseEvent>) => {
-
-        // Set that we should be drawing and store offsets of the mouse position
-        setIsDrawing(true);
-        setPreviousPosition({
-            offsetX: event.clientX,
-            offsetY: event.clientY
-        });
+    /** Performs drawing saving workflow */
+    const onSaveClick = () => {
+        setSaveOccurred(true);
     };
 
-    /** Function that handles drawing the figure as the mouse moves */
-    const onMouseMove = (event: React.MouseEvent<HTMLCanvasElement, MouseEvent>) => {
-
-        // If drawing should be occurring, track the start and stop locations of the figure
-        if (isDrawing) {
-            const currentPosition = {
-                offsetX: event.clientX,
-                offsetY: event.clientY
-            };
-
-            // Store the position of the mouse to the figure and draw it on canvas
-            figureLine.push(currentPosition);
-            performDrawing(currentPosition);
-        }
-    };
-
-    /** Function that handles pausing drawing */
-    const pauseDrawing = (event: React.MouseEvent<HTMLCanvasElement, MouseEvent>) => {
-        if (isDrawing) {
-            setIsDrawing(false);
-        }
-    };
-
-    /** Performs drawing and stroking on the canvas */
-    const performDrawing = (newPosition: { offsetX: number, offsetY: number }) => {
-        const { offsetX, offsetY } = newPosition;
-
-        context!.beginPath();
-        context!.strokeStyle = 'red';
-
-        // Draw a line from the previous position to the current one
-        context!.moveTo(previousPosition.offsetX, previousPosition.offsetY);
-        context!.lineTo(offsetX, offsetY);
-        context!.stroke();
-
-        setPreviousPosition({ offsetX, offsetY });
+    /** Performs drawing reset workflow */
+    const onResetClick = () => {
+        setResetOccurred(true);
     };
 
     return (
         <div className="draw">
-            <canvas className="draw__canvas"
-                style={{ background: 'black' }}
-                ref={canvasElement}
-                onMouseDown={onMouseDown}
-                onMouseMove={onMouseMove}
-                onMouseLeave={pauseDrawing}
-                onMouseUp={pauseDrawing}
-            />
+            <p>Draw to your hearts content!</p>
+
+            {/** Canvas for drawing */}
+            <DrawCanvas
+                saveOccurred={saveOccurred}
+                resetOccurred={resetOccurred} />
+
+            {/** Action buttons */}
+            <div className="draw__actions">
+                <button
+                    className="draw__reset"
+                    onClick={onResetClick}>
+                    Reset Drawing
+                </button>
+                <button
+                    className="draw__save"
+                    onClick={onSaveClick}>
+                    Save Drawing
+                </button>
+            </div>
         </div>
     )
 };
---
+++ src/components/draw/drawCanvas.tsx
@@ -0,0 +1,104 @@
+import React, { useState, useRef, useEffect } from 'react';
+
+interface IDrawCanvasProps {
+    saveOccurred: boolean,
+    resetOccurred: boolean,
+};
+
+const DrawCanvas = (props: IDrawCanvasProps) => {
+
+    const canvasElement = useRef<HTMLCanvasElement>(null);
+    const [context, setContext] = useState<CanvasRenderingContext2D>();
+    const [isDrawing, setIsDrawing] = useState(false);
+    const [previousPosition, setPreviousPosition] = useState<any>({});
+
+    const figureLine = [];
+
+    /** Sets up our canvas and context on component mounting */
+    useEffect(() => {
+        canvasElement.current!.width = 1000;
+        canvasElement.current!.height = 800;
+        setContext(canvasElement.current!.getContext('2d')!);
+    }, []);
+
+    /** Once context is ready, styke it */
+    useEffect(() => {
+        if (context) {
+            context.lineJoin = 'round';
+            context.lineCap = 'round';
+            context.lineWidth = 5;
+        }
+    }, [context]);
+
+    /** Handles resetting the canvas */
+    useEffect(() => {
+        if (props.resetOccurred) {
+            console.log(`A reset occurred`);
+            context!.clearRect(0, 0, canvasElement.current!.width, canvasElement.current!.height);
+        }
+    }, [props.resetOccurred, context]);
+
+    /** Function that handles drawing on mouse down */
+    const onMouseDown = (event: React.MouseEvent<HTMLCanvasElement, MouseEvent>) => {
+
+        // Set that we should be drawing and store offsets of the mouse position
+        setIsDrawing(true);
+        setPreviousPosition({
+            offsetX: event.nativeEvent.offsetX,
+            offsetY: event.nativeEvent.offsetY
+        });
+    };
+
+    /** Function that handles drawing the figure as the mouse moves */
+    const onMouseMove = (event: React.MouseEvent<HTMLCanvasElement, MouseEvent>) => {
+
+        // If drawing should be occurring, track the start and stop locations of the figure
+        if (isDrawing) {
+            const currentPosition = {
+                offsetX: event.nativeEvent.offsetX,
+                offsetY: event.nativeEvent.offsetY
+            };
+
+            // Store the position of the mouse to the figure and draw it on canvas
+            figureLine.push(currentPosition);
+            performDrawing(currentPosition);
+        }
+    };
+
+    /** Function that handles pausing drawing */
+    const pauseDrawing = (event: React.MouseEvent<HTMLCanvasElement, MouseEvent>) => {
+        if (isDrawing) {
+            setIsDrawing(false);
+        }
+    };
+
+    /** Performs drawing and stroking on the canvas */
+    const performDrawing = (newPosition: { offsetX: number, offsetY: number }) => {
+        const { offsetX, offsetY } = newPosition;
+
+        context!.beginPath();
+        context!.strokeStyle = 'red';
+
+        // Draw a line from the previous position to the current one
+        context!.moveTo(previousPosition.offsetX, previousPosition.offsetY);
+        context!.lineTo(offsetX, offsetY);
+        context!.stroke();
+
+        setPreviousPosition({ offsetX, offsetY });
+    };
+
+    return (
+        <div className="draw-canvas">
+            <canvas className="canvas"
+                style={{ background: 'black' }}
+                ref={canvasElement}
+                onMouseDown={onMouseDown}
+                onMouseMove={onMouseMove}
+                onMouseLeave={pauseDrawing}
+                onMouseUp={pauseDrawing}
+            />
+        </div>
+    )
+};
+
+export default DrawCanvas;
