0071ee1f5213aad1ba5356e3387f68883f462056
Christopher D Jativa <cjativa@barnesfoundation.org>
2020-12-28 15:06:29 -0500

    middleware for validating the jwt token and adding userId to the request

--- server/api/middleware/authenticationMiddleware.ts
+++ server/api/middleware/authenticationMiddleware.ts
@@ -5,25 +5,46 @@ import Config from '../../utils/config';
 export const isSessionAuthenticated = async (request: express.Request, response: express.Response, next: express.NextFunction) => {
 
     // Check if there's a bearer token in header
-    const { authorization } = request.headers;
+    const token = getBearerToken(request.headers.authorization);
 
     // If there's no token on the request, reject immediately
-    if (!authorization) {
+    if (!token) {
         response
             .status(403)
-            .json({ message: 'Invalid or missing authorization token' });
+            .json('Invalid or missing authorization token');
     }
 
     // Else, there's a token. Let's see if it's valid
     else {
-        const userPayload = jwt.verify(authorization, Config.jwtSecretKey);
+        const userPayload = jwt.verify(token, Config.jwtSecretKey) as { userId: number, username: string };
 
         // If there token is invalid (i.e. no payload), clear it and let the client know
         if (!userPayload) {
             response.status(403).json('Invalid or missing authorization token');
         }
 
-        // Else, token is valid, allow continuation of the request
+        // Else, token is valid so let's attach the user id to the request
+        else { request.userId = userPayload.userId; }
+
         next();
+    }
+};
+
+const getBearerToken = (authorizationText: string): string => {
+
+    // If there was no value in the authorization header
+    if (!authorizationText) {
+        throw Error('Invalid or missing authorization token');
+    }
+
+    // Otherwise, try to pick out the token
+    try {
+        const token = authorizationText.substring(authorizationText.indexOf(' ') + 1);
+        return token;
+    }
+
+    catch (error) {
+        console.log('An error occurred extracting the bearer token', error);
+        throw Error('Invalid or missing authorization token');
     }
 };
